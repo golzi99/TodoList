80e66f3670be6f32c4d407a8a04c4f6cfd7d10df
Pavel <golzi99@gmail.com>
2024-08-12 22:02:59 +0300

    12.08.2024 fix button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,11 +14,13 @@ export const Button = ({title}: ButtonPropsType) => {
 
 const StyledButton = styled.button`
   position: relative;
+  display: inline-block;
   background-color: ${myTheme.colors.lightGreen};
   padding: 4px;
   border: ${myTheme.colors.borderColor} 1px solid;
-  border-radius: 4px;
+  border-radius: 16px;
   z-index: 1;
+  overflow: hidden;
 
   &:before {
     content: '';
@@ -28,7 +30,6 @@ const StyledButton = styled.button`
     width: 100%;
     height: 100%;
     z-index: -1;
-    border-radius: 4px;
     background-color: ${myTheme.colors.lightPurple};
     transition: transform 0.4s ease;
     transform: scaleX(0);
