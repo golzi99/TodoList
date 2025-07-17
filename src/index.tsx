8f835e12962614c4355830792f3acbf3e9074063
golzi99 <golzi99@gmail.com>
2025-07-18 00:01:37 +0300

    basename

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import App from 'app/App'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename={'TodoList'}>
     <Provider store={store}>
       <App />
     </Provider>
